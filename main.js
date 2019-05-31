940e2f73c3b940bd29afc1a5feb3efb4ddc0d750
ThunderDev1 <sgranger1@gmail.com>
2019-05-31 14:28:42 +0200

    Removed axios and did some refactoring

--- main.js
+++ main.js
@@ -1,48 +1,88 @@
-const fs = require('fs');
-const axios = require('axios');
+const fs = require("fs");
+const fetch = require("node-fetch");
 
-async function run() {
+/*
+  // the emoji code is in the image link
+  // eg. https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/72/google/3/grinning-face_1f600.png
+  // code is 1f600
+*/
+const extractEmojipediaCodes = pageBody => {
+  const matchedEmojiCodes = pageBody.match(/(1f\S{3}-?){1,2}(?=\.png)/gm);
+  distinctCodes = Array.from(new Set(matchedEmojiCodes));
+  console.log(`Found ${distinctCodes.length} emoji codes in source file`);
+  return distinctCodes;
+};
 
-  const response = await axios.get('https://emojipedia.org/google/android-5.0/')
+const getEmojiMartNames = (codes, emojiMartConfig) => {
+  const emojiMartEmojis = Object.keys(emojiMartConfig.emojis);
+  const emojiNames = emojiMartEmojis.map(emojiName => {
+    const emojiMartCode = emojiMartConfig.emojis[emojiName].b.toLowerCase();
+    return codes.includes(emojiMartCode) && emojiName;
+  });
 
-  const matchedEmojiCodes = response.data.match(/(1f\S{3}-?){1,2}(?=\.png)/gm);
-  // Remove duplicates
-  const codes = Array.from(new Set(matchedEmojiCodes));
+  console.log(`Found ${emojiNames.length} matching emojis in emoji-mart file`);
+  return emojiNames;
+};
 
-  console.log(`Found ${codes.length} emoji codes in source file`);
-
-  var emojiMartConfig = JSON.parse(fs.readFileSync('./node_modules/emoji-mart/data/google.json', 'utf8'));
-
-  const emojiNames = [];
-
+const getRequiredEmojis = (emojiMartConfig, requiredEmojiNames) => {
   const emojiMartEmojis = Object.keys(emojiMartConfig.emojis);
 
-  emojiMartEmojis.forEach(emojiName => {
-    const emojiMartCode = emojiMartConfig.emojis[emojiName].b.toLowerCase();
-    if (codes.includes(emojiMartCode)) {
-      emojiNames.push(emojiName);
+  return emojiMartEmojis.reduce((requiredEmojis, emojiName) => {
+    if (requiredEmojiNames.includes(emojiName)) {
+      requiredEmojis[emojiName] = { ...emojiMartConfig.emojis[emojiName] };
     }
+    return requiredEmojis;
+  }, {});
+};
+
+const getRequiredCategories = (emojiMartCategories, requiredEmojiNames) => {
+  const requiredCategories = emojiMartCategories.map(category => {
+    const { emojis, otherProps } = category;
+
+    return {
+      ...otherProps,
+      emojis: emojis.filter(emojiName => requiredEmojiNames.includes(emojiName))
+    };
   });
+  return requiredCategories;
+};
 
-  console.log(`Found ${emojiNames.length} corresponding emojis in emoji-mart file`);
+const extract = pageBody => {
+  const emojiMartConfig = JSON.parse(
+    fs.readFileSync("./node_modules/emoji-mart/data/google.json", "utf8")
+  );
 
-  let removedCount = 0;
+  const emojipediaCodes = extractEmojipediaCodes(pageBody);
 
-  emojiMartEmojis.forEach(emojiName => {
-    if (!emojiNames.includes(emojiName)) {
-      delete emojiMartConfig.emojis[emojiName];
-      removedCount++;
-    }
-  });
+  const requiredEmojiNames = getEmojiMartNames(
+    emojipediaCodes,
+    emojiMartConfig
+  );
 
-  console.log(`Removed ${removedCount} emojis from emoji-mart file`);
+  const requiredEmojis = getRequiredEmojis(emojiMartConfig, requiredEmojiNames);
 
-  emojiMartConfig.categories.forEach(category => {
-    category.emojis = category.emojis.filter((emoji) => emojiNames.includes(emoji));
-  });
+  const requiredCategories = getRequiredCategories(
+    emojiMartConfig.categories,
+    requiredEmojiNames
+  );
 
-  const filteredConfig = JSON.stringify(emojiMartConfig);
-  fs.writeFileSync(`filteredConfig-${Date.now()}.json`, filteredConfig, 'utf8');
-}
+  const newConfig = {
+    compressed: true,
+    emojis: requiredEmojis,
+    categories: requiredCategories
+  };
+
+  fs.writeFileSync(
+    `filteredConfig-${Date.now()}.json`,
+    JSON.stringify(newConfig),
+    "utf8"
+  );
+};
+
+const run = () => {
+  fetch("https://emojipedia.org/google/android-5.0/")
+    .then(response => response.text())
+    .then(body => extract(body));
+};
 
 run();
